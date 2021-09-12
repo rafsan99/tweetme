07ae8d34a6df99265b8f8294fea7049e1951786d
rafsan99 <rafsan.redwan69@gmail.com>
2021-09-12 16:36:13 +0600

    REST APIView

--- tweets/views.py
+++ tweets/views.py
@@ -1,16 +1,27 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+        # "content": obj.content,
+        # "image_path": obj.image.url
+    }
+
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
