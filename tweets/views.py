f1ec26e8f1914537b4c26b51af26c9a3fc51d22e
rafsan99 <rafsan.redwan69@gmail.com>
2021-09-14 01:15:01 +0600

    Handling AJAX requests

--- tweets/views.py
+++ tweets/views.py
@@ -14,13 +14,17 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
-    print("next url", next_url)
+    
     if form.is_valid():
         obj = form.save(commit=False)
         #do other form realated logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
+
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
