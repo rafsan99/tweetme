54868da03b323a57d13ecac90fdcb878d65a1136
rafsan99 <rafsan.redwan69@gmail.com>
2021-09-13 18:18:02 +0600

    Form and URL redirect

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2c20k5%h7z8(jx3#^-u(=a7#l^y!+dhc&ij4ekif2e9)x35h4i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
