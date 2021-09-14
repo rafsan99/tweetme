d205a41d993ad82f9ccd5b4e7f73767ff422b086
rafsan99 <rafsan.redwan69@gmail.com>
2021-09-15 02:37:46 +0600

    creating user

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,11 @@
 import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     
