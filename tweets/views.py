22a3d81c8fd814fe5d235c57472ec1fc143c4b3c
rafsan99 <rafsan.redwan69@gmail.com>
2021-09-21 03:56:24 +0600

    Retweeting Logic

--- tweets/views.py
+++ tweets/views.py
@@ -65,6 +65,7 @@ def tweet_action_view(request, *args, **kwargs):
         data = serializer.validated_data
         tweet_id = data.get("id")
         action = data.get("action")
+        content = data.get("content")
 
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
@@ -77,9 +78,13 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            #this is to do
-            pass
-
+            new_tweet = Tweet.objects.create(
+                user=request.user,
+                parent=obj,
+                content=content,
+                )
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
     return Response({}, status=200)
 
 
